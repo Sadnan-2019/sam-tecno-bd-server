c37f0f04bd63fdb4b247e73ee6d2fa29e326d8c8
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2024-07-04 00:17:41 +0600

    mongo

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT || 5000
-
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello SamTecNo World!')
-})
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sam-tecno-bd.7ehypcr.mongodb.net/?retryWrites=true&w=majority&appName=sam-tecno-bd`;
+
+console.log(uri)
+console.log(uri)
+
+app.get("/", (req, res) => {
+  res.send("Hello SamTecNo World!");
+});
 
 app.listen(port, () => {
-  console.log(`SAMTECNO listening on port ${port}`)
-})
+  console.log(`SAMTECNO listening on port ${port}`);
+});
