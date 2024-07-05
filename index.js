8b906fb55c2984851d9043b946ed2cf6386cc4a6
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2024-07-05 23:10:40 +0600

    mongo connected done

--- index.js
+++ index.js
@@ -8,11 +8,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sam-tecno-bd.7ehypcr.mongodb.net/?retryWrites=true&w=majority&appName=sam-tecno-bd`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sam-tecno-bd.7ehypcr.mongodb.net/?retryWrites=true&w=majority&appName=sam-tecno-bd`;
 
-console.log(uri)
-console.log(uri)
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run(){
+  try {
+    await client.connect();
+    console.log("database connected");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
   res.send("Hello SamTecNo World!");
